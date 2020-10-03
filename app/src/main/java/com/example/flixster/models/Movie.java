7c3ff4b35de7165c2e42b4d88a9f23400e024b78
keyuyan1145 <keyuyan01@gmail.com>
2020-10-03 01:32:28 -0400

    part2 commits

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,21 +3,30 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overview;
     String backDrop;
+    int movieId;
+    double rating;
+
+    // Empty constructor needed for Parceler library
+    public Movie() { }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backDrop = jsonObject.getString("backdrop_path");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -42,5 +51,11 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() { return rating; }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
